821a7588f07909c9103398751864fbfcfa9d5d6b
Mike Ulvila <mike.ulvila@premisehealth.com>
2016-11-14 07:44:25 -0600

    more work

--- src/app/recipes/recipe-list/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input} from '@angular/core';
+import { Recipe } from '../recipe';
 @Component({
   selector: 'rb-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  templateUrl: './recipe-item.component.html'
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  recipeId: number;
 
   constructor() { }
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe";
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [];
+  recipe = new Recipe('Dummy', 'Dummy description', 'http://www.coastalpacificre.com/wp-content/uploads/2015/03/placeholder_male1.jpg')
   constructor() { }
 
   ngOnInit() {
